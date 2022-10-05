620e525af390a0d9a0b4f20108e6410ae04f563b
Junior Daliessi <juniordaliessi@gmail.com>
2022-10-05 05:06:24 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,81 @@
+import './style.css'
+import MovieCard from "components/MovieCard"
 import Pagination from "components/Pagination"
 
-function Listing(){
-    return(
-        <Pagination/>
-    )
+function Listing() {
+    return (
+
+        <>
+            <Pagination />
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-xl-3 col-lg-4 col-md-6 col-sm-12 mb-3">
+                        <div className='transition-dsmovie-card'>
+                            <MovieCard />
+                        </div>
+                    </div>
+                    <div className="col-xl-3 col-lg-4 col-md-6 col-sm-12 mb-3">
+                        <div className='transition-dsmovie-card'>
+                            <MovieCard />
+                        </div>
+                    </div>
+                    <div className="col-xl-3 col-lg-4 col-md-6 col-sm-12 mb-3">
+                        <div className='transition-dsmovie-card'>
+                            <MovieCard />
+                        </div>
+                    </div>
+                    <div className="col-xl-3 col-lg-4 col-md-6 col-sm-12 mb-3">
+                        <div className='transition-dsmovie-card'>
+                            <MovieCard />
+                        </div>
+                    </div>
+                    <div className="col-xl-3 col-lg-4 col-md-6 col-sm-12 mb-3">
+                        <div className='transition-dsmovie-card'>
+                            <MovieCard />
+                        </div>
+                    </div>
+                    <div className="col-xl-3 col-lg-4 col-md-6 col-sm-12 mb-3">
+                        <div className='transition-dsmovie-card'>
+                            <MovieCard />
+                        </div>
+                    </div>
+                    <div className="col-xl-3 col-lg-4 col-md-6 col-sm-12 mb-3">
+                        <div className='transition-dsmovie-card'>
+                            <MovieCard />
+                        </div>
+                    </div>
+                    <div className="col-xl-3 col-lg-4 col-md-6 col-sm-12 mb-3">
+                        <div className='transition-dsmovie-card'>
+                            <MovieCard />
+                        </div>
+                    </div>
+                    <div className="col-xl-3 col-lg-4 col-md-6 col-sm-12 mb-3">
+                        <div className='transition-dsmovie-card'>
+                            <MovieCard />
+                        </div>
+                    </div>
+                    <div className="col-xl-3 col-lg-4 col-md-6 col-sm-12 mb-3">
+                        <div className='transition-dsmovie-card'>
+                            <MovieCard />
+                        </div>
+                    </div>
+                    <div className="col-xl-3 col-lg-4 col-md-6 col-sm-12 mb-3">
+                        <div className='transition-dsmovie-card'>
+                            <MovieCard />
+                        </div>
+                    </div>
+                    <div className="col-xl-3 col-lg-4 col-md-6 col-sm-12 mb-3">
+                        <div className='transition-dsmovie-card'>
+                            <MovieCard />
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        </>
+
+    );
 }
 export default Listing
